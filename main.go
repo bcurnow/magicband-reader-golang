e7e8405ce6596a415030f225659953b6a3fdae13
“Brian <bcurnow@users.noreply.github.com>
2021-08-21 07:21:59 +0100

    Added logging and config initialization

--- main.go
+++ main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	log "github.com/sirupsen/logrus"
+
 	"github.com/bcurnow/magicband-reader/audio"
-	//"github.com/bcurnow/magicband-reader/led"
+	"github.com/bcurnow/magicband-reader/led"
 )
 
 func main() {
+	config := Config{}
+	if err := config.Init(); err != nil {
+		panic(err)
+	}
+
+	log.Debug("Initializing LED")
+	ledController := led.Controller{OuterRing: 144, InnerRing: 0}
+	if err := ledController.Init(); err != nil {
+		panic(err)
+	}
+
+	log.Debug("Initializing Audio")
 	audioController := audio.Controller{}
 	if err := audioController.Init(); err != nil {
 		panic(err)
@@ -15,5 +29,7 @@ func main() {
 		panic(err)
 	}
 
+	log.Debug("Playing audio")
 	audioController.Play(audioStream)
+	log.Debug("Exiting")
 }
