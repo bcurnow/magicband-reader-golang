f2cdb4cb4f8c00464caa92c0565b84f109950e88
“Brian <bcurnow@users.noreply.github.com>
2021-08-27 17:41:52 +0100

    Moved the Handler registration logic hear to avoid issues initializing the handlers in the wrong order

--- context/context.go
+++ context/context.go
@@ -1,17 +1,27 @@
 package context
 
 import (
+	"errors"
+	"fmt"
+	"sort"
+
 	log "github.com/sirupsen/logrus"
 
 	"github.com/bcurnow/magicband-reader/audio"
 	"github.com/bcurnow/magicband-reader/auth"
 	"github.com/bcurnow/magicband-reader/config"
+	"github.com/bcurnow/magicband-reader/event"
 	"github.com/bcurnow/magicband-reader/led"
 )
+
+type Handler interface {
+	Handle(event event.Event) error
+}
 
 var (
 	AudioController audio.Controller
 	AuthController  auth.Controller
+	Handlers        map[int]Handler
 	LEDController   led.Controller
 	State           map[string]interface{}
 )
@@ -36,8 +46,36 @@ func init() {
 
 	AudioController = audioController
 	AuthController = authController
+	Handlers = make(map[int]Handler)
 	LEDController = ledController
 	State = make(map[string]interface{})
+}
+
+func RegisterHandler(priority int, handler Handler) error {
+	existingHandler, exists := Handlers[priority]
+
+	if exists {
+		return errors.New(fmt.Sprintf("Handler '%T' already registered with priority %v", existingHandler, priority))
+	}
+	Handlers[priority] = handler
+	log.Debugf("Handler '%T' registered with priority %v", handler, priority)
+	return nil
+}
+
+func SortedHandlers() []Handler {
+	keys := make([]int, 0, len(Handlers))
+
+	for key := range Handlers {
+		keys = append(keys, key)
+	}
+
+	sort.Ints(keys)
+
+	sorted := make([]Handler, 0, len(Handlers))
+	for _, key := range keys {
+		sorted = append(sorted, Handlers[key])
+	}
+	return sorted
 }
 
 func Close() error {
