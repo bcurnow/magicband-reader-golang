37d323fde97631c4f51208a4facb09abaa1cfa98
“Brian <bcurnow@users.noreply.github.com>
2021-08-27 15:11:36 +0100

    Updated logging levels, added handling for IRQ error and removed sleep workaround

--- reader.go
+++ reader.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/hex"
-	"fmt"
 	"strings"
 	"sync"
 	"time"
@@ -35,7 +34,7 @@ type reader struct {
 }
 
 func NewDefaultReader() (*reader, error) {
-	log.Debug("Creating new default reader")
+	log.Trace("Creating new default reader")
 	reader, err := NewReader("", rpi.P1_22, rpi.P1_18)
 	if err != nil {
 		return nil, err
@@ -44,7 +43,7 @@ func NewDefaultReader() (*reader, error) {
 }
 
 func NewReader(spiPort string, resetPin gpio.PinOut, irqPin gpio.PinIn) (*reader, error) {
-	log.Debug(fmt.Sprintf("Creating new reader with spiPort='%v', resetPin='%v', irqPin='%v'", spiPort, resetPin, irqPin))
+	log.Tracef("Creating new reader with spiPort='%v', resetPin='%v', irqPin='%v'", spiPort, resetPin, irqPin)
 	var reader = reader{}
 
 	// Make sure periph is initialized.
@@ -62,7 +61,8 @@ func NewReader(spiPort string, resetPin gpio.PinOut, irqPin gpio.PinIn) (*reader
 	if err != nil {
 		return nil, err
 	}
-	// Setting the antenna signal strength.
+
+	// Setting the antenna signal strength per the example code
 	mfrc522.SetAntennaGain(5)
 
 	reader.mfrc522 = mfrc522
@@ -71,16 +71,22 @@ func NewReader(spiPort string, resetPin gpio.PinOut, irqPin gpio.PinIn) (*reader
 }
 
 func (r *reader) Close() {
-	log.Debug("Closing Reader")
+	log.Trace("Closing Reader")
 	r.closed = true
 	// Halt the device before we lock, this will stop any in-progress reads
+	log.Trace("Halting the device")
 	r.mfrc522.Halt()
+	log.Trace("Device halted")
 
 	//Make sure to lock before we close the underlying port or we'll cause a SIGSEGV
+	log.Trace("Acquiring the lock")
 	r.Lock()
 	defer r.Unlock()
+	log.Trace("Lock acquired")
 	//Close the underlying SPI port
+	log.Trace("Closing the portCloser")
 	r.portCloser.Close()
+	log.Trace("Reader closed")
 }
 
 func (r *reader) UID(timeout time.Duration) (string, error) {
@@ -108,6 +114,9 @@ func (r *reader) UID(timeout time.Duration) (string, error) {
 
 			// If main thread timed out just exit.
 			if timedOut {
+				// This will only be called after this function exits as the only thing that sets
+				// timedOut is the defered anonymous function above, don't worry about writing to
+				// any of the channels, there's nothing left to read from them.
 				return
 			}
 
@@ -118,11 +127,22 @@ func (r *reader) UID(timeout time.Duration) (string, error) {
 					haltChannel <- err
 					return
 				}
+
+				if err.Error() == "mfrc522 lowlevel: IRQ error" {
+					// I have no idea why this error is sometimes generate, it happens when there's not even a card to trigger the reading
+					// I don't have time to debug and it actually doesn't appear to happen very often or impact future reading so I'm just
+					// going to return here and hope for the best until such time as I can debug more in depth
+					//TODO why is this necessary?
+					haltChannel <- err
+					return
+				}
+
 				// Not sure what the error is, we should just try to read again
-				log.Trace(fmt.Sprintf("reader.UID: Unexpected error: %v", err))
+				log.Warnf("reader.UID: Unexpected error: %v", err)
 				continue
 			}
 
+			log.Tracef("Read UID %v", uid)
 			// We got a UID, we're done
 			uidChannel <- uid
 			return
@@ -134,8 +154,6 @@ func (r *reader) UID(timeout time.Duration) (string, error) {
 		case <-timer.C:
 			return "", TimeoutError{}
 		case uid := <-uidChannel:
-			// TODO Remove this, this is a crazy work around
-			time.Sleep(1 * time.Second)
 			return strings.ToUpper(hex.EncodeToString(uid)), nil
 		case <-haltChannel:
 			return "", nil
