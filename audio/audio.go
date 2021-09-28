09727756f98c7bd860018f8e0211dd4ffa336658
“Brian <bcurnow@users.noreply.github.com>
2021-09-28 19:18:08 +0100

    Moved sound validation logic here since we know the most about it, updated to use a cache for the audio, this is the first step to making the audio more dynamic. Updated the Load and Play methods so they deal with a buffer and not a streamer, we need to re-create the streamer from the buffer before playing each time because otherwise the sound doesn't play (as the stream has already been fully read).

--- audio/audio.go
+++ audio/audio.go
@@ -1,7 +1,11 @@
 package audio
 
 import (
+	"errors"
+	"fmt"
+	"io/fs"
 	"os"
+	"path"
 
 	"github.com/faiface/beep"
 	"github.com/faiface/beep/effects"
@@ -15,28 +19,45 @@ const (
 )
 
 type Controller interface {
-	Load(soundFile string) (beep.Streamer, error)
-	Play(streamer beep.Streamer)
+	Load(soundFile string) (*beep.Buffer, error)
+	Play(buffer *beep.Buffer)
+	AuthorizedSound() string
+	ReadSound() string
+	UnauthorizedSound() string
 }
 
 type controller struct {
-	sampleRate beep.SampleRate
-	Volume     float64
-	Base       float64
+	cache             Cache
+	sampleRate        beep.SampleRate
+	volume            float64
+	base              float64
+	authorizedSound   string
+	readSound         string
+	unauthorizedSound string
 }
 
-func NewController(volume float64, base float64) (Controller, error) {
+func NewController(volume float64, base float64, cache Cache, authorizedSound string, readSound string, unauthorizedSound string) (Controller, error) {
 	log.Trace("Creating new audio.Controller")
+
 	c := controller{
-		Volume: volume,
-		Base:   base,
+		cache:             cache,
+		volume:            volume,
+		base:              base,
+		authorizedSound:   authorizedSound,
+		readSound:         readSound,
+		unauthorizedSound: unauthorizedSound,
 	}
+
+	if err := c.validateSoundConfig(cache.CacheDir()); err != nil {
+		return nil, err
+	}
+
 	c.handleDefaults()
 	return &c, nil
 }
 
-func (c *controller) Load(soundFile string) (beep.Streamer, error) {
-	f, err := os.Open(soundFile)
+func (c *controller) Load(soundFile string) (*beep.Buffer, error) {
+	f, err := c.cache.Load(soundFile)
 	if err != nil {
 		return nil, err
 	}
@@ -69,28 +90,66 @@ func (c *controller) Load(soundFile string) (beep.Streamer, error) {
 	// Read the file into memory, these are very small files
 	buffer := beep.NewBuffer(format)
 	buffer.Append(streamer)
+	return buffer, nil
+}
 
-	streamer = buffer.Streamer(0, buffer.Len())
+func (c *controller) Play(buffer *beep.Buffer) {
+	streamer := buffer.Streamer(0, buffer.Len())
 
 	volume := &effects.Volume{
 		Streamer: streamer,
-		Base:     c.Base,
-		Volume:   c.Volume,
+		Base:     c.base,
+		Volume:   c.volume,
 		Silent:   false,
 	}
-	return volume, nil
-}
 
-func (c *controller) Play(streamer beep.Streamer) {
 	done := make(chan bool)
-	speaker.Play(beep.Seq(streamer, beep.Callback(func() {
+	speaker.Play(beep.Seq(volume, beep.Callback(func() {
 		close(done)
 	})))
 	<-done
 }
 
+func (c *controller) AuthorizedSound() string {
+	return c.authorizedSound
+}
+
+func (c *controller) ReadSound() string {
+	return c.readSound
+}
+
+func (c *controller) UnauthorizedSound() string {
+	return c.unauthorizedSound
+}
+
 func (c *controller) handleDefaults() {
-	if c.Base == 0 {
-		c.Base = defaultBase
+	if c.base == 0 {
+		c.base = defaultBase
 	}
+}
+
+func (c *controller) validateSoundConfig(soundDir string) error {
+	if err := validateFileExists(soundDir, "sound-dir"); err != nil {
+		return err
+	}
+
+	if err := validateFileExists(path.Join(soundDir, c.authorizedSound), "authorized-sound"); err != nil {
+		return err
+	}
+
+	if err := validateFileExists(path.Join(soundDir, c.readSound), "read-sound"); err != nil {
+		return err
+	}
+
+	if err := validateFileExists(path.Join(soundDir, c.unauthorizedSound), "unauthorized-sound"); err != nil {
+		return err
+	}
+	return nil
+}
+
+func validateFileExists(file string, name string) error {
+	if _, err := os.Stat(file); errors.Is(err, fs.ErrNotExist) {
+		return fmt.Errorf("Invalid value for %v: '%v'. %v", name, file, err)
+	}
+	return nil
 }
